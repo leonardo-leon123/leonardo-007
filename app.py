47ff852625e4dd91e13755f7364735c222bee842
leonardo-leon123 <62914773+leonardo-leon123@users.noreply.github.com>
2020-10-16 21:03:40 -0500

    Add files via upload
    Primer commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for, request
+import requests
+from pprint import pprint
+import os
+subscription_key = "33aa870e5d29431887c452b2b3d95d17"
+
+endpoint = "https://elsy.cognitiveservices.azure.com/"
+
+languages_url = endpoint + "/text/analytics/v3.0/languages"
+
+app = Flask(__name__)
+
+@app.route('/007',methods=["POST","GET"])
+def index():
+    if request.method == "POST":
+        idioma = request.form['descripcion'] 
+        documents= {
+            "documents": [
+            {
+            "id": "1",
+            "text": idioma
+            }]
+        } 
+        headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+        response = requests.post(languages_url, headers=headers, json=documents)
+        languages = response.json()
+        idioma_encontrado = languages['documents'][0]['detectedLanguage']['name']   
+        pprint(idioma_encontrado)            
+    return render_template('007.html',idioma_encontrado = idioma_encontrado)
